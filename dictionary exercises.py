f8bb0ffbc64003e62942cb0202fdb9a93641ab08
AbdiwahabAli955 <113719490+AbdiwahabAli955@users.noreply.github.com>
2022-09-22 15:15:43 -0600

    Add files via upload

---
+++ dictionary exercises.py
@@ -0,0 +1,8 @@
+#Create a dictionary called "about_me" that contains the following information: 
+#your name (string format), age (float format), year of study (integer format), 
+#and favorite foods in a list (list of strings).
+
+about_me = {"name" : "abdiwahab", "age" : 20.0, "year of study" : 4, "favorite foods":["pizza","burgers",'pasta'] }
+print(about_me)
+print(type(about_me))
+print(len(about_me))
---
+++ yourname.py
@@ -0,0 +1,25 @@
+#print("a")
+#print("b")
+#print("d")
+#print("i")
+#print("w")
+#print("a")
+#print("h")
+#print("a")
+#print("b")
+
+
+
+letter1 = "z"
+letter2 = "b"
+letter3 = "d"
+letter4 = "i"
+letter5 = "w"
+letter6 = "a"
+letter7 = "h"
+letterX = letter1
+letter9 = "b"
+
+print(letterX)
+print(letter1)
+
